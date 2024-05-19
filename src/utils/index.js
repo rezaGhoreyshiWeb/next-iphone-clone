9b80f797e7e0aa2b5e96bc7d71a9853d8b0e4e34
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2024-05-19 15:10:46 +0330

    feat: add hero.jsx component

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,14 +2,12 @@ import hero from "../assets/images/hero.jpeg";
 
 export const heroImg = hero;
 
-import hmv from "../assets/videos/hero.mp4";
-import smallmv from "../assets/videos/smallHero.mp4";
-import highlightFirstmv from "../assets/videos/highlight-first.mp4";
-import highlightSectmv from "../assets/videos/hightlight-third.mp4";
-import highlightThirdmv from "../assets/videos/hightlight-sec.mp4";
-import highlightFourthmv from "../assets/videos/hightlight-fourth.mp4";
-import exploremv from "../assets/videos/explore.mp4";
-import framemv from "../assets/videos/frame.mp4";
+// import highlightFirstmv from "../assets/videos/highlight-first.mp4";
+// import highlightSectmv from "../assets/videos/hightlight-third.mp4";
+// import highlightThirdmv from "../assets/videos/hightlight-sec.mp4";
+// import highlightFourthmv from "../assets/videos/hightlight-fourth.mp4";
+// import exploremv from "../assets/videos/explore.mp4";
+// import framemv from "../assets/videos/frame.mp4";
 
 import apple from "../assets/images/apple.svg";
 import search from "../assets/images/search.svg";
@@ -29,14 +27,13 @@ import explore2 from "../assets/images/explore2.jpg";
 import chip from "../assets/images/chip.jpeg";
 import frame from "../assets/images/frame.png";
 
-export const heroVideo = hmv;
-export const smallHeroVideo = smallmv;
-export const highlightFirstVideo = highlightFirstmv;
-export const highlightSecondVideo = highlightSectmv;
-export const highlightThirdVideo = highlightThirdmv;
-export const highlightFourthVideo = highlightFourthmv;
-export const exploreVideo = exploremv;
-export const frameVideo = framemv;
+
+// export const highlightFirstVideo = highlightFirstmv;
+// export const highlightSecondVideo = highlightSectmv;
+// export const highlightThirdVideo = highlightThirdmv;
+// export const highlightFourthVideo = highlightFourthmv;
+// export const exploreVideo = exploremv;
+// export const frameVideo = framemv;
 
 export const appleImg = apple;
 export const searchImg = search;
